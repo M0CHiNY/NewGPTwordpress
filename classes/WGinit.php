d552a2e32e8352385b03da9e7e50883b4f6b3273
light <y.khrystiuk@dev-branch.com>
2023-02-13 20:34:29 +0200

    #05 - changes classes

--- classes/WGinit.php
+++ classes/WGinit.php
@@ -1,12 +1,14 @@
 <?php
 namespace classes;
 
+// Define the WGinit class
 class WGinit {
 
     /**
      * Constructor method that adds an action hook to "admin_menu"
      */
     public function __construct() {
+        // Add an action hook to "admin_menu"
         add_action( 'admin_menu', [$this, 'create_menu'] );
     }
 
@@ -14,38 +16,63 @@ class WGinit {
      * Creates the menu page and submenu page for the plugin
      */
     public function create_menu() {
+        // Add the main menu page for the plugin
         add_menu_page(
-            __( 'WriteGenie', 'text' ), // Plugin name
+            __( 'WriteGenie', 'text' ), // Page title
             __( 'WriteGenie', 'text' ), // Menu title
-            'manage_options',
+            'manage_options', // Capability
             'writegenie_board', // URL address
-            [$this, 'wgBoard'], // Callback function for the main menu page
-            'dashicons-admin-users',
-            500
+            [$this, 'wgBoard'], // Callback function
+            'dashicons-admin-users', // Icon
+            500 // Menu position
         );
 
+        // Add the submenu page for the plugin
         add_submenu_page(
             'writegenie_board', // Parent menu name
+            __( 'Settings', 'text' ), // Page title
             __( 'Settings', 'text' ), // Submenu title
-            __( 'Settings', 'text' ), // Submenu title
-            'manage_options',
+            'manage_options', // Capability
             'writegenie_settings', // URL address
-            [$this,'wgSettings'] // Callback function for the submenu page
+            [$this,'wgSettings'] // Callback function
         );
     }
 
     /**
-     * Callback function that includes the "wg-board.php" file when the "writegenie_board" page is accessed
+     * Callback function for the main menu page
+     * Includes the "wg-board.php" file
      */
     public function wgBoard(){
-        include plugin_dir_path(__FILE__)."Pages/wg-board.php";
+        // Include the "wg-board.php" file
+        include $this->pathBack(1)."/wg-board.php";
     }
 
     /**
-     * Callback function that includes the "wg-settings.php" file when the "writegenie_settings" page is accessed
+     * Callback function for the submenu page
+     * Includes the "wg-settings.php" file
      */
     public function wgSettings(){
-        include plugin_dir_path(__FILE__)."Pages/wg-settings.php";
+        // Include the "wg-settings.php" file
+        include $this->pathBack(1)."/wg-settings.php";
+    }
+
+    /**
+     * Returns the root path of the plugin directory
+     *
+     * @param int $num The number of parent directories to remove from the current directory
+     *
+     * @return string The root path of the plugin directory
+     */
+    private function pathBack($num){
+        $num = 0;
+        // Get the plugin directory path
+        $root_path = plugin_dir_path( __FILE__ );
+
+        // Remove the specified number of parent directories
+        for ($i=0; $i<=$num; $i++) {
+            $root_path = dirname($root_path);
+        }
+        return $root_path;
     }
 
 }
